a13d7ae2d951b35c481c5978e72d8f6c990c7ed8
esc20936 <esc20936@uvg.edu.gt>
2022-10-01 07:02:53 -0600

    Create Scene

---
+++ src/Canvas.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import * as THREE from 'three';
+import './Canvas.css';
+
+function Canvas() {  
+
+  return (
+    <>
+      <canvas className="webgl"></canvas>   
+    </>
+  )
+}
+
+export default Canvas;
